8cbfed5d49535ce49a97cec05f740db4c4b0dffc
Jimmy Nguyen <55465528+PrettyLegit@users.noreply.github.com>
2024-05-20 23:33:34 -0400

    Update investment_calculator.go
    - used const, scan, and pointer to memory address

--- investment_calculator/investment_calculator.go
+++ investment_calculator/investment_calculator.go
@@ -6,12 +6,24 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	fmt.Println("Investment Calculator")
-	var investmentAmount float64 = 1000
+	var investmentAmount float64
 	expectedReturnRate := 5.5
-	years := 10.0
+	var years float64
+
+	fmt.Print("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter number of years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println("future value: ", futureValue)
+	fmt.Println("futureRealValue: ", futureRealValue)
 }
